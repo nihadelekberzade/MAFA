36fb49268f30aaf9aa19fb8723c941c78b6f2864
unknown <elekberzadeh333@gmail.com>
2020-09-22 18:39:46 +0400

    Team page is working and fully responsive

--- src/components/Team/Team.js
+++ src/components/Team/Team.js
@@ -8,57 +8,63 @@ import { images } from '../../img/images'
 
 export default function Team() {
 
-    const team  = [
+    // eslint-disable-next-line no-unused-vars
+    const team = [
         {
-            name:'ДМИТРИЙ МУЗЫЧКА',
-            profession:'head of front end',
-            src:images.dimaM,
-            linkedin:'https://www.linkedin.com/in/nihad-elekberzade-550959185/',
-            email:'elekberzadeh333@gmail.com'
+            name: 'ДМИТРИЙ МУЗЫЧКА',
+            profession: 'head of front end',
+            src: images.dimaM,
+            linkedin: 'https://www.linkedin.com/in/nihad-elekberzade-550959185/',
+            email: 'elekberzadeh333@gmail.com'
         },
         {
-            name:'ДМИТРИЙ ЗАПИСОЧНЫЙ',
-            profession:'head of front end',
-            src:images.dimaZ,
-            linkedin:'https://www.linkedin.com/in/nihad-elekberzade-550959185/',
-            email:'elekberzadeh333@gmail.com'
+            name: 'ДМИТРИЙ ЗАПИСОЧНЫЙ',
+            profession: 'head of front end',
+            src: images.dimaZ,
+            linkedin: 'https://www.linkedin.com/in/nihad-elekberzade-550959185/',
+            email: 'elekberzadeh333@gmail.com'
         },
         {
-            name:'ДМИТРИЙ ЗАПИСОЧНЫЙ',
-            profession:'head of front end',
-            src:images.dimaM,
-            linkedin:'https://www.linkedin.com/in/nihad-elekberzade-550959185/',
-            email:'elekberzadeh333@gmail.com'
+            name: 'ДМИТРИЙ ЗАПИСОЧНЫЙ',
+            profession: 'head of front end',
+            src: images.dimaM,
+            linkedin: 'https://www.linkedin.com/in/nihad-elekberzade-550959185/',
+            email: 'elekberzadeh333@gmail.com'
         },
     ]
-
 
     return (
         <div className='team'>
             <ul className='team-list'>
-                <li className='team-item'>
-                    <Developer
-                        name='ДМИТРИЙ ЗАПИСОЧНЫЙ'
-                        profession='head of front end'
-                        src={images.dimaM}
-                        linkedin='https://www.linkedin.com/in/nihad-elekberzade-550959185/'
-                        email='elekberzadeh333@gmail.com'
-                    />
-                    <Developer
-                        name='ДМИТРИЙ ЗАПИСОЧНЫЙ'
-                        profession='head of front end'
-                        src={images.dimaZ}
-                        linkedin='https://www.linkedin.com/in/nihad-elekberzade-550959185/'
-                        email='elekberzadeh333@gmail.com'
-                    />
-                    <Developer
-                        name='ЕВГЕНИЯ КУКУШКИНА'
-                        profession='head of front end'
-                        src={images.evgeniya}
-                        linkedin='https://www.linkedin.com/in/nihad-elekberzade-550959185/'
-                        email='elekberzadeh333@gmail.com'
-                    />
-                </li>
+                <div className='column'>
+                    <li className='item item-dimaZ'>
+                        <Developer
+                            name='ДМИТРИЙ ЗАПИСОЧНЫЙ'
+                            profession='head of front end'
+                            src={images.dimaM}
+                            linkedin='https://www.linkedin.com/in/nihad-elekberzade-550959185/'
+                            email='elekberzadeh333@gmail.com'
+                        />
+                    </li>
+                    <li className='item item-dimaM'>
+                        <Developer
+                            name='ДМИТРИЙ ЗАПИСОЧНЫЙ'
+                            profession='head of front end'
+                            src={images.dimaZ}
+                            linkedin='https://www.linkedin.com/in/nihad-elekberzade-550959185/'
+                            email='elekberzadeh333@gmail.com'
+                        />
+                    </li>
+                    <li className='item item-evgeniya'>
+                        <Developer
+                            name='ЕВГЕНИЯ КУКУШКИНА'
+                            profession='head of front end'
+                            src={images.evgeniya}
+                            linkedin='https://www.linkedin.com/in/nihad-elekberzade-550959185/'
+                            email='elekberzadeh333@gmail.com'
+                        />
+                    </li>
+                </div>
             </ul>
         </div>
     )
