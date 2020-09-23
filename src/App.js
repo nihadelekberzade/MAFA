b5aab6373870a06adf101b51f05798a6127e9bd7
unknown <elekberzadeh333@gmail.com>
2020-09-23 21:08:40 +0400

    Project is ready for deployment,real photos and contact links needed

--- src/App.js
+++ src/App.js
@@ -5,14 +5,15 @@ import Home from './components/Home/Home';
 import Sidebar from './components/Sidebar/Sidebar';
 import Team from './components/Team/Team';
 import './global.css'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom'
 
 function App() {
+  
   return (
     <Router>
       <div className="App">
-
         <Switch>
+          <Redirect exact from="/" to="searchDashboard" />
           <Route path='/home' component={Home} />
           <Route path='/team' component={Team} />
           <Route path='/contacts' component={Contacts} />
