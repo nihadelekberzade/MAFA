d635f890a5b603c314c52c2d2e9a9a04cf1559c2
unknown <elekberzadeh333@gmail.com>
2020-09-26 17:28:56 +0400

    app.js changed

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import './app.css'
 import Contacts from './components/Contacts/Contacts';
 import Home from './components/Home/Home';
 import Sidebar from './components/Sidebar/Sidebar';
