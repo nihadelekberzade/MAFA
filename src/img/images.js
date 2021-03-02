bb4790a208e6ff2688dcc163f706d564b7e01a95
unknown <elekberzadeh333@gmail.com>
2021-03-02 18:33:43 +0400

    some pictures compressed

--- src/img/images.js
+++ src/img/images.js
@@ -1,8 +1,8 @@
-import dimaZ from './team/DimaZ.jpg'
-import dimaM from './team/DimaM.jpg'
-import evgeniya from './team/Evgeniya.jpg'
-import contacts_pic from './contacts-pic.png'
-import services_bg from './services-bg.jpg'
+import dimaZ from './team/DimaZ.webp'
+import dimaM from './team/DimaM.webp'
+import evgeniya from './team/Evgeniya.webp'
+import contacts_pic from './contacts-pic.webp'
+import services_bg from './services-bg.webp'
 import logo_black from './logo_black.svg'
 import logo_white from './logo_white.svg'
 import c from './shapes/c.png'
@@ -12,10 +12,10 @@ import o from './shapes/o.png'
 
 
 
-export const images = 
-Object.freeze({
-    dimaZ,dimaM,evgeniya,
-    contacts_pic,services_bg,
-    logo_white,logo_black,
-    c,cu,x,o
-});
+export const images =
+    Object.freeze({
+        dimaZ, dimaM, evgeniya,
+        contacts_pic, services_bg,
+        logo_white, logo_black,
+        c, cu, x, o
+    });
